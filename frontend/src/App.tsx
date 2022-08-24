81b9add1858235cf25596fac0876f0cc39d9f76c
Christoph Weber <chrisweber1991@gmail.com>
2022-08-24 15:54:47 +0200

    register Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Game from './Game';
+import RegisterPage from './pages/RegisterPage';
+import WelcomePage from './pages/WelcomePage';
 
 export default function App() {
 
@@ -8,7 +10,9 @@ export default function App() {
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<Game/>}/>
+                    <Route path='/' element={<WelcomePage/>}/>
+                    <Route path='/register' element={<RegisterPage/>}/>
+                    <Route path='/game' element={<Game/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
